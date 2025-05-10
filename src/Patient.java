fa61a87f3e057150a70561a6fe8e22d987206a83
aisyahhafizar <aisyah05.ah@gmail.com>
2025-05-10 14:57:27 +0800

    Update Patient.java

--- src/Patient.java
+++ src/Patient.java
@@ -1,4 +1,55 @@
 package PACKAGE_NAME;
 
-public class Patient {
+public class Patient extends User
+{
+    private String patientID;
+    private String patientName;
+    private String patientNumber;
+    private String gender;
+    private int age;
+
+    Patient ()
+    {
+        this.patientID=patientID;
+        this.patientName=patientName;
+        this.patientNumber=patientNumber;
+        this.gender=gender;
+        this.age=age;
+    }
+
+    public String getPatientID()
+    {
+        return patientID;
+    }
+
+    public String getPatientName()
+    {
+        return patientName;
+    }
+
+    public String getPatientNumber()
+    {
+        return patientNumber;
+    }
+
+    public String getGender()
+    {
+        return gender;
+    }
+
+    public int getAge()
+    {
+        return age;
+    }
+
+    @Override
+    public void displayDetails()
+    {
+        System.out.println("Patient ID : " + patientID);
+        System.out.println("Patient Name : " + patientName);
+        System.out.println("Patient Phone Number : " + patientNumber);
+        System.out.println("Patient Age : " + age);
+        System.out.println("Gender : " + gender);
+
+    }
 }

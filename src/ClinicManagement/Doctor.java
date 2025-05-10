e3c49bd56c3ebb515161c4bd2c2f5a9e64e84e51
nayliali <71825431+nayliali@users.noreply.github.com>
2025-05-11 00:48:36 +0800

    Update Doctor.java

--- src/ClinicManagement/Doctor.java
+++ src/ClinicManagement/Doctor.java
@@ -2,5 +2,25 @@ package ClinicManagement;
 
 public class Doctor extends User {
 
-  private String doc_name
+  private String doc_name;
+  private String doc_id;
+  private String doc_number;
+  private String availabiity
+  private double chgperappt; 
+
+  public Doctor(String doc_name, String doc_id, String doc_number, String availability, double chgperappt){
+    this.doc_name = doc_name;
+    this.doc_id = doc_id;
+    this.doc_number = doc_number;
+    this.availability = availability;
+    this.chgperappt = chgperappt;
+  }
+
+  public 
+
+
+
+  
 }
+
+  

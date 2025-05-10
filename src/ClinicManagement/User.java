eba443989bfeaf7aac9a6e01f19f8bd5175472d9
aisyahhafizar <aisyah05.ah@gmail.com>
2025-05-10 19:19:09 +0800

    Update User.java

--- src/ClinicManagement/User.java
+++ src/ClinicManagement/User.java
@@ -2,26 +2,16 @@ package ClinicManagement;
 
 public abstract class User
 {
-    protected String patientID;
+    
     protected String patientName;
     protected String patientNumber;
-    protected String gender;
-    protected int age;
-
-    User (String patientID,String patientName,String patientNumber,String gender,int age)
+    
+    User (String patientName,String patientNumber)
     {
-        this.patientID=patientID;
         this.patientName=patientName;
         this.patientNumber=patientNumber;
-        this.gender=gender;
-        this.age=age;
     }
-
-    public String getPatientID()
-    {
-        return patientID;
-    }
-
+    
     public String getPatientName()
     {
         return patientName;
@@ -31,17 +21,6 @@ public abstract class User
     {
         return patientNumber;
     }
-
-    public String getGender()
-    {
-        return gender;
-    }
-
-    public int getAge()
-    {
-        return age;
-    }
-
-
+    
     public abstract void displayDetails();
 }

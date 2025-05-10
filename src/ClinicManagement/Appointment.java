8382e933e0d9c9607406ef74d60ec07ed750a4b7
nayliali <71825431+nayliali@users.noreply.github.com>
2025-05-11 02:39:41 +0800

    Update Appointment.java

--- src/ClinicManagement/Appointment.java
+++ src/ClinicManagement/Appointment.java
@@ -1,4 +1,33 @@
 package ClinicManagement;
 
 public class Appointment {
+  private String appointmentID;
+  private Date appointmentDate;
+  private String appointmentStatus;
+
+  public Appointment(String appointmentID, Date appointmentDate, String appointmentStatus){
+    this.appointmentID = appointmentID;
+    this.appointmentDate = appointmentDate;
+    this.appointmentStatus = appointmentStatus;
+  }
+
+  public String getAppointmentID(){
+    return appointmentID;
+  }
+
+  public Date getAppointmentDate(){
+    return appointmentDate;
+  }
+
+  public String getAppointmentStatus(){
+    return appointmentStatus;
+  }
+
+  public void displayAppointment(){
+    System.out.println("Appointmemnt ID : "+appointmentID);
+    System.out.println("Appointmemnt Date : "+appointmentDate);
+    System.out.println("Appointmemnt status : "+appointmentStatus);
+  }
+  
 }
+  

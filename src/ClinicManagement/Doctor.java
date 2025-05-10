c31fd44e82c77a14857a338eaee82a3d0a8ab7a0
nayliali <71825431+nayliali@users.noreply.github.com>
2025-05-11 01:27:39 +0800

    Update Doctor.java

--- src/ClinicManagement/Doctor.java
+++ src/ClinicManagement/Doctor.java
@@ -5,10 +5,11 @@ public class Doctor extends User {
   private String doc_name;
   private String doc_id;
   private String doc_number;
-  private String availabiity
+  private String availabiity;
   private double chgperappt; 
 
-  public Doctor(String doc_name, String doc_id, String doc_number, String availability, double chgperappt){
+  public Doctor(String username, String password, String doc_name, String doc_id, String doc_number, String availability, double chgperappt){
+    super(username, password);
     this.doc_name = doc_name;
     this.doc_id = doc_id;
     this.doc_number = doc_number;
@@ -16,11 +17,30 @@ public class Doctor extends User {
     this.chgperappt = chgperappt;
   }
 
-  public 
+  public void viewMedicalHistory(ArrayList[] medicalHistory, String patient_id){
+    System.out.println("Medical history for patient id : "+patient_id);
+    for (String history : medicalHistory){
+      System.out.println(history);
+    }
+  }
 
+  public void viewAppointment(ArrayList[] appointments, String patient_id){
+    System.out.println("Appointments for patient id : "+patient_id);
+    for (String appointment : appointments){
+      System.out.println(appointment);
+    }
+  }
 
+  public void updatePatientsVisit(String patient_id, String visitDetail){
+    System.out.println("Updating visit details for patient id : "+patient_id);
+    System.out.println("New visit details : "+visitDetails);
+  }
 
-  
+  public void updatePatientPrescription(String patient_id, String prescriptionDetails){
+    System.out.println("Updating prescription for Patient ID: " + patientId);
+    System.out.println("New Prescription Details: " + prescriptionDetails);
+  }
+    
 }
 
   

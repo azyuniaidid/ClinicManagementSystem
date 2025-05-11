9b2dae4078a7e90e5b5483ac80ec2647718ca59c
wanthwothree <wsorayaz@gmail.com>
2025-05-11 20:15:05 +0800

    Update MedicalHistory.java

--- src/ClinicManagement/MedicalHistory.java
+++ src/ClinicManagement/MedicalHistory.java
@@ -2,4 +2,11 @@ package ClinicManagement;
 
 public class MedicalHistory 
 {
+  private String diagnosis;
+  private String prescrition;
+  private String date;
+
+  MedicalHistory (String diagnosis, String prescription, String date)
+  {
+    
 }

9cd06e8f2573b4fbbe78ec8989f3cc5f6a7b76d4
azyuniaidid <nurazyuniaidid@gmail.com>
2025-05-24 18:24:39 +0800

    hi

---
+++ src/ClinicManagement/MainAppClinic.java
@@ -0,0 +1,48 @@
+package ClinicManagement;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+
+public class MainAppClinic extends Application {
+
+    private Stage primaryStage;
+    private Scene scene;
+    private final RegisterPatient registerPatient = new RegisterPatient();
+    private final RegisterDoctor registerDoctor = new RegisterDoctor();
+    private final CreateAppointment createAppointment = new CreateAppointment();
+    private final AddMedicalHistory addMedicalHistory = new AddMedicalHistory();
+    private final GenerateBill generateBill = new GenerateBill();
+
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        Parent root = getFirstPanelView();
+        scene = new Scene(root, 800, 700);
+        primaryStage.setTitle("JavaFX Multiple UI Panels Example");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    public void setScene(Parent view) {
+        scene.setRoot(view);
+    }
+    public Parent getFirstPanelView() {
+        return registerPatient.getView(this);
+    }
+    public Parent getSecondPanelView() {
+        return registerDoctor.getView(this);
+    }
+    public Parent getThirdPanelView() {
+        return createAppointment.getView(this);
+    }
+    public Parent getFourthPanelView() {
+        return addMedicalHistory.getView(this);
+    }
+    public Parent getFifthPanelView() {
+        return generateBill.getView(this);
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}

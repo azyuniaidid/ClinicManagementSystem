96f41664c99f50b2bcbccf04070429319fc06f0f
aisyahhafizar <aisyah05.ah@gmail.com>
2025-05-25 19:46:43 +0800

    Update CreateAppointment.java

--- src/ClinicManagement/CreateAppointment.java
+++ src/ClinicManagement/CreateAppointment.java
@@ -6,7 +6,6 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.*;
 import javafx.scene.Parent;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;

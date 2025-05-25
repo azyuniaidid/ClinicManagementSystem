f0e7c8522eed3ae36ba0dc0f46ecf5f492a222c7
nayliali <71825431+nayliali@users.noreply.github.com>
2025-05-25 17:21:10 +0800

    Update Doctor.java

--- src/ClinicManagement/Doctor.java
+++ src/ClinicManagement/Doctor.java
@@ -1,10 +1,17 @@
 package ClinicManagement;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
 public class Doctor extends User {
 
   private String doctorID;
   private String specialization;
   private double chgperappt; 
+  private final ArrayList<Doctor> doctors =new ArrayList<>();
+  private final String filePath = "doctor.txt";
 
   Doctor(String name,String doctorID,String phoneNumber,String specialization,double chgperappt )
     {
@@ -12,25 +19,33 @@ public class Doctor extends User {
         this.doctorID = doctorID;
         this.specialization = specialization;
         this.chgperappt = chgperappt;
+        System.out.println("Doctor instance has created");
     }
 
-    public String getDoctorID()
-    {
-
+    public String getDoctorID() {
         return doctorID;
     }
 
-    public String getSpecialization()
-    {
+    public void setDoctorID(String doctorID) {
+        this.doctorID = doctorID;
+    }
 
+    public String getSpecialization() {
         return specialization;
     }
 
-    public double getChgperappt()
-    {
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
 
+    public double getChgperappt() {
         return chgperappt;
     }
+
+    public void setChgperappt(double chgperappt) {
+        this.chgperappt = chgperappt;
+    }
+
 
     @Override
     public void displayDetails()
@@ -44,12 +59,31 @@ public class Doctor extends User {
     }
 
     @Override
-    public String toString()
-    {
+    public String toString(){
 
         return doctorID + " " + name + " " + phoneNumber + " " + specialization + " " + chgperappt ;
     }
     
+    public void addDoctor(Doctor doctor){
+        
+        doctors.add(doctor);
+        System.out.println("Doctor added to the array");
+        
+        try (PrintWriter writer = new PrintWriter(new FileWriter(filePath))){
+            for (Doctor Doctor : doctors){
+                writer.println(doctor);
+            }
+            System.out.println("Data writtern to doctor.txt");
+        }catch (IOException e){
+            System.out.println("Error writing to file");
+            e.printStackTrace();
+        }
+    }
+    
+    public ArrayList<Doctor> getDoctors(){
+        return doctors;
+    }
+      
 }
 
   

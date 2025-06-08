2f69413e5767dd6c951ae63d212fef06ed763cc5
aisyahhafizar <aisyah05.ah@gmail.com>
2025-06-08 15:04:38 +0800

    Update RegisterPatient.java

--- src/ClinicManagement/RegisterPatient.java
+++ src/ClinicManagement/RegisterPatient.java
@@ -1,12 +1,14 @@
-package ClinicManagement;
+package com.example.oop_uiproject;
 
-import ClinicManagement.MainAppClinic;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.*;
 import javafx.scene.Parent;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
@@ -22,15 +24,20 @@ public class RegisterPatient
 {
     public Parent getView(MainAppClinic app)
     {
-        //patient banner
-        Text sceneTitle = new Text("PATIENT REGISTRATION");
-        sceneTitle.setFont(Font.font("Tahoma", FontWeight.BOLD, 20));
+        //Clinic banner
+        // Load an image (from URL or local resource)
+        Image image = new Image("file:/Users/aisyahhafizar/Downloads/3.png");
+        ImageView bannerPatient = new ImageView(image);
+        bannerPatient.setFitHeight(200);
+        bannerPatient.setFitWidth(1400);
 
+        //add banner
         HBox banner = new HBox();
         banner.setAlignment(Pos.CENTER);
-        banner.setPadding(new Insets(30)); //size of the green banner
-        banner.getChildren().add(sceneTitle);
-        banner.setStyle("-fx-background-color: #ADD378;");
+        banner.setPadding(new Insets(30));
+        banner.setMaxWidth(Double.MAX_VALUE); // Let it expand fully
+
+        banner.getChildren().add(bannerPatient);
 
         //grid banner layout
         GridPane grid = new GridPane();
@@ -38,14 +45,15 @@ public class RegisterPatient
         grid.setHgap(10);
         grid.setVgap(10);
         grid.setPadding(new Insets(25, 25, 25, 25));
-        grid.setPrefWidth(500);
-
+        grid.setPrefWidth(650);
 
         //patient id
         Label ptlabel = new Label("Patient ID");
         ptlabel.setFont(Font.font("Arial", FontWeight.BOLD,15));
         grid.add(ptlabel, 0, 0);
         TextField ptid = new TextField();
+        ptid.setPromptText("Enter Patient ID");
+        ptid.setPrefWidth(100);
         grid.add(ptid,0,1);
 
         //patient name
@@ -53,6 +61,8 @@ public class RegisterPatient
         ptname.setFont(Font.font("Arial", FontWeight.BOLD,15));
         grid.add(ptname, 0, 2);
         TextField patientName = new TextField();
+        patientName.setPromptText("Enter Patient Name");
+        patientName.setPrefWidth(500);
         grid.add(patientName,0,3);
 
         //phone number
@@ -60,6 +70,8 @@ public class RegisterPatient
         number.setFont(Font.font("Arial", FontWeight.BOLD,15));
         grid.add(number, 0, 4);
         TextField phoneNumber = new TextField();
+        phoneNumber.setPromptText("Enter Phone Number");
+        phoneNumber.setPrefWidth(400);
         grid.add(phoneNumber,0,5);
 
         //gender
@@ -68,37 +80,47 @@ public class RegisterPatient
         grid.add(gender, 0, 6);
 
         ComboBox<String> genderBox = new ComboBox<>();
+        genderBox.setPromptText("Select Gender");
         genderBox.getItems().addAll("Male", "Female");
-        genderBox.setPrefWidth(100);
+        genderBox.setPrefWidth(300);
         grid.add(genderBox, 0, 7);
 
         //age
         Label age = new Label("Age");
         age.setFont(Font.font("Arial", FontWeight.BOLD,15));
         grid.add(age, 0, 8);
-        TextField ages = new TextField();
-        grid.add(ages,0,9);
 
-        //register and cancel button
-        Button register = new Button("Register");
-        grid.add(register,5, 10);
-        Button cancel = new Button("Cancel");
-        grid.add(cancel,4, 10);
-
-        final Text
-        actiontarget = new Text();
-
-        grid.add(actiontarget, 5, 11);
+        ComboBox<Integer> ageBox = new ComboBox<>();
+        for (int i = 0; i <= 100; i++) // age range
+        {
+            ageBox.getItems().add(i);
+        }
+        ageBox.setPrefWidth(100);
+        ageBox.setPromptText("Age");
+        grid.add(ageBox, 0, 9);
 
         //value box which shows all patient information
+        VBox infoBox = new VBox(10);
         Label view = new Label("Patient Information");
         view.setFont(Font.font("Arial", FontWeight.BOLD,15));
         grid.add(view, 0, 12);
 
+        //text area
         TextArea area = new TextArea();
-        area.setPrefHeight(100);
-        area.setPrefWidth(300);
-        grid.add(area, 0, 13, 2, 1); //i2 height i3 width
+        area.setPrefSize(500, 300);
+        infoBox.getChildren().addAll(view,area);
+
+        HBox buttonBox = new HBox(10); // spacing between buttons
+        buttonBox.setAlignment(Pos.BOTTOM_RIGHT); // align buttons to the left
+        Button register = new Button("Register");
+        Button cancel = new Button("Cancel");
+        buttonBox.getChildren().addAll(cancel,register);
+        grid.add(buttonBox, 0, 10, 2, 1); //i2=going right i3=going down
+
+        final Text actiontarget = new Text();
+        grid.add(actiontarget, 0, 11, 2, 1);
+        GridPane.setHalignment(actiontarget, HPos.RIGHT);
+        grid.setPrefWidth(600);
 
         //once register is clicked, values will show
         register.setOnAction(new EventHandler<ActionEvent>()
@@ -106,7 +128,7 @@ public class RegisterPatient
             @Override
             public void handle(ActionEvent actionEvent)
             {
-                System.out.println("Successfully Registered!");
+                System.out.println("Successfully Registered Patient Information!");
 
                 //patient is registered
                 actiontarget.setFill(Color.GREEN);
@@ -125,7 +147,7 @@ public class RegisterPatient
                 String patientGender = genderBox.getValue();
 
                 //read age
-                String patientAge = ages.getText();
+                String patientAge = age.getText();
 
                 String msg = "Patient ID :  " + id + "\n"
                         +"Patient Name : " + name + "\n"
@@ -156,7 +178,10 @@ public class RegisterPatient
             @Override
             public void handle(ActionEvent e)
             {
-                actiontarget.setFill(Color.DARKRED);
+                actiontarget.setFill(Color.RED);
+                ptid.setText(""); //text fields
+                patientName.setText("");
+                phoneNumber.setText("");
                 actiontarget.setText("Registration is Cancelled");
             }
         });
@@ -175,7 +200,7 @@ public class RegisterPatient
             @Override
             public void handle(ActionEvent event)
             {
-                RegisterDoctorPanel regDoctor = new RegisterDoctorPanel();
+                RegisterDoctor regDoctor = new RegisterDoctor();
                 app.setScene(regDoctor.getView(app));
             }
         });
@@ -223,6 +248,14 @@ public class RegisterPatient
             }
         });
 
+        HBox form = new HBox(30);
+        form.getChildren().addAll(grid);
+        form.setAlignment(Pos.TOP_CENTER);
+
+        HBox info = new HBox(30);
+        info.getChildren().add(infoBox);
+        form.setAlignment(Pos.TOP_RIGHT);
+
         //buttons at the side
         VBox sideBtns = new VBox(10);
         sideBtns.setPrefWidth(180);
@@ -230,21 +263,23 @@ public class RegisterPatient
         sideBtns.setAlignment(Pos.TOP_LEFT);
 
         //size of letters, size of buttons
-        regDoctorBtn.setStyle("-fx-font-size: 14px; -fx-min-width: 150px;");
-        createApptBtn.setStyle("-fx-font-size: 14px; -fx-min-width: 150px;");
-        medHistoryBtn.setStyle("-fx-font-size: 14px; -fx-min-width: 150px;");
-        genBillBtn.setStyle("-fx-font-size: 14px; -fx-min-width: 150px;");
-        mainPageBtn.setStyle("-fx-font-size: 14px; -fx-min-width: 150px;");
+        regDoctorBtn.setStyle("-fx-font-size: 14px; -fx-min-width: 200px; -fx-min-height: 50px");
+        createApptBtn.setStyle("-fx-font-size: 14px; -fx-min-width: 200px; -fx-min-height: 50px");
+        medHistoryBtn.setStyle("-fx-font-size: 14px; -fx-min-width: 200px; -fx-min-height: 50px");
+        genBillBtn.setStyle("-fx-font-size: 14px; -fx-min-width: 200px; -fx-min-height: 50px");
+        mainPageBtn.setStyle("-fx-font-size: 14px; -fx-min-width: 200px; -fx-min-height: 50px");
 
-        HBox contentLayout = new HBox();
+
+        HBox contentLayout = new HBox(30);
         contentLayout.setPadding(new Insets(20));
-        contentLayout.getChildren().addAll(sideBtns, grid);
-        contentLayout.setAlignment(Pos.CENTER);
+        contentLayout.getChildren().addAll(sideBtns, grid,infoBox);
+        contentLayout.setAlignment(Pos.CENTER_LEFT);
 
         sideBtns.getChildren().addAll(regDoctorBtn, createApptBtn, medHistoryBtn, genBillBtn, mainPageBtn);
 
-        VBox fullLayout = new VBox();
-        fullLayout.getChildren().addAll(banner, contentLayout);
+        VBox fullLayout = new VBox(10);
+        fullLayout.getChildren().addAll(bannerPatient,contentLayout);
+        fullLayout.setPadding(new Insets(10));
 
         return fullLayout;
     }
